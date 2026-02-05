da5a5845dbfae3e65108a02a3bf4c4fc32e849f9
shanellobo <shanelllobo06@gmail.com>
2026-02-05 10:38:05 +0530

    Add files via upload

---
+++ Register.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistrationForm.Models
+{
+    public class Register
+    {
+        [Required(ErrorMessage = "Name is Required")]
+        [RegularExpression(@"^[A-Za-z]+$",
+            ErrorMessage = "Name must contain only alphabets")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is Required")]
+        [EmailAddress(ErrorMessage = "invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "password is Required")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{6,}$",
+            ErrorMessage = "password must be atleast 6 characters and contain at least one lowercase, one uppercase, one digit, one special character")]
+        [MinLength(6, ErrorMessage = "Password must be atleast 6 characters")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "age is Required")]
+        [Range(18, 60, ErrorMessage = "age must be between 18 and 60")]
+        public string Age { get; set; }
+
+    }
+}
---
+++ RegisterController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistrationForm.Models;
+
+namespace RegistrationForm.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult Index(Register user)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content(
+                    $"Registration Successful\nname: {user.Name}\nEmail: {user.Email}\nAge: {user.Age}\n");
+            }
+            return View(user);
+        }
+    }
+}
+    
