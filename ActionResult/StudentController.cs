be65d067bc0a57cb18fe14016d37b7d02a83a36d
shanellobo <shanelllobo06@gmail.com>
2026-02-05 10:58:09 +0530

    Add files via upload

---
+++ ActionResult/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace program3.Controllers
+{
+    public class StudentController : Controller
+    {
+        [Route("Details/{id}")]
+        public IActionResult Details(int id)
+        {
+            return Content("Student id is: " + id);
+        }
+
+        [Route("Marks/{id}/{subject}")]
+        public IActionResult Marks(int id, string subject)
+        {
+            return Content("student id: " + id + ", subject: " + subject);
+        }
+    }
+}
