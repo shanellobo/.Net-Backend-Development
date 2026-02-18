2bdf880abb9f1c549f36d9c743ad4a658beddad4
shanellobo <shanelllobo06@gmail.com>
2026-02-18 10:02:53 +0530

    Add files via upload

---
+++ staticfiles/Program.cs
@@ -0,0 +1,15 @@
+namespace StatichtmlFiles
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+
+            app.Run();
+        }
+    }
+}
