4503b6ff752476401be8f8d8055cafe686efe577
shanellobo <shanelllobo06@gmail.com>
2026-02-18 10:12:52 +0530

    Add files via upload

---
+++ student/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentApp.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string USN { get; set; }
+    }
+}
---
+++ student/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentApp.Models;
+
+namespace StudentApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Creating a student object
+            Student student = new Student
+            {
+                Name = "shanell",
+                USN = "NU24UCA066"
+            };
+
+            return View(student);
+        }
+    }
+}
