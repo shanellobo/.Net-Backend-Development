d84c7c765b95feb0537483da29ca7cb33e8da995
shanellobo <shanelllobo06@gmail.com>
2026-02-18 09:59:30 +0530

    Add files via upload

---
+++ rasorsyntax/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace RasorSyntax.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ rasorsyntax/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RasorSyntax.Models;
+
+namespace RasorSyntax.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product { Id = 1, Name= "laptop", Price = 55000 },
+                new Product { Id = 2, Name= "Mobile", Price = 25000 },
+                new Product { Id = 3, Name= "HeadPhones", Price = 3000 },
+                new Product { Id = 4, Name= "Speaker", Price = 15000 },
+                new Product { Id = 5, Name= "TV", Price = 30000 },
+            };
+            return View(products);
+        }
+    }
+}
