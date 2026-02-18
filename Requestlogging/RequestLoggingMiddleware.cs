1f296754f2551ddeb609112dea2c7b1030d1b075
shanellobo <shanelllobo06@gmail.com>
2026-02-18 10:01:11 +0530

    Add files via upload

---
+++ Requestlogging/RequestLoggingMiddleware.cs
@@ -0,0 +1,24 @@
+﻿namespace progrm2
+{
+    public class RequestLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestLoggingMiddleware(RequestDelegate next) 
+        {
+            _next = next;
+
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("----Incoming Request------");
+            Console.WriteLine($"time: {DateTime.Now}");
+            Console.WriteLine($"Method: {context.Request.Method}");
+            Console.WriteLine($"path: {context.Request.Path}");
+            Console.WriteLine("---------------------------");
+
+            await _next(context);
+        }
+
+    }
+}
