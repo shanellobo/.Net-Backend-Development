eee93825d845ea8677e1d609a9f07e52ed42cc24
shanellobo <shanelllobo06@gmail.com>
2026-02-19 10:24:44 +0530

    Add files via upload

---
+++ applicationsetting/ConfigController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace applicationsetting.Controllers
+{
+    public class ConfigController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public ConfigController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.AppName 
+                = _configuration["AppSettings:ApplicationName"];
+
+            ViewBag.Version
+                 = _configuration["AppSettings:Version"];
+
+            ViewBag.Developer
+                 = _configuration["AppSettings:Developer"];
+
+            return View();
+        }
+    }
+}
